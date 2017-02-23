d3ddd781a7e4dba366d5b292767b5d76bcedde0a
AkashGutha <akash.gutha@outlook.com>
2017-02-23 17:51:33 +0530

    gulp and anfular setup, index file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+const gulp  = require('gulp');
+const babel = require('gulp-babel');
 
-gulp.task('default', [], function () {
+gulp.task('default', ['transpileHTML','transpileCSS','transpileJS'], function () {
     
+});
+
+gulp.task('transpileJS',[], function(){
+    gulp.src('src/**/*.js')
+        .pipe(babel({
+            presets : ['es2015']
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('transpileHTML',[], function(){
+    gulp.src('src/**/*.html')
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('transpileCSS',[], function(){
+    gulp.src('src/**/*.css')
+        .pipe(gulp.dest('build'));
 });
